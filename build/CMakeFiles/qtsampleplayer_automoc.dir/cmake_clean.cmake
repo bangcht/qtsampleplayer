ba34a70ce6780bb8e04803fff3fd6e79342061f4
Bang Nguyen <bangcht@gmail.com>
2015-09-11 01:23:58 +0700

    del

--- build/CMakeFiles/qtsampleplayer_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "qtsampleplayer_automoc.cpp"
-  "CMakeFiles/qtsampleplayer_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/qtsampleplayer_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
